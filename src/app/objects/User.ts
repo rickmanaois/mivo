68a32cecb0c3912893566449819413e8ef5c680b
Jethru Balarbar <marx.balarbar@tooltwist.com>
2020-04-30 20:41:57 +0800

    Update User.ts

--- src/app/objects/User.ts
+++ src/app/objects/User.ts
@@ -8,6 +8,16 @@
   fullName: string;
   address: string;
   expiryDay: number;
+  selectedAgent: {
+    agentCode: number;
+    agentName: string;
+    documentCode: string;
+    documentType: string;
+    documentName: string;
+    agentType: number;
+    agentTypeName: string;
+    agentAddress: string;
+  };
 
   token: string;
   constructor(init ? : Partial < User > ) {
